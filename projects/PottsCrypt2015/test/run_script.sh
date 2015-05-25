b6245d38a15dea5c38992a59ee3a385331ea3911
jameso@comlab.ox.ac.uk <jameso@comlab.ox.ac.uk@b5f69653-2bf5-0310-8201-b0dafb8e1405>
2015-05-25 02:06:04 +0000

    [PottsCrypt2015] Tydying for release

--- projects/PottsCrypt2015/test/run_script.sh
+++ projects/PottsCrypt2015/test/run_script.sh
@@ -7,10 +7,10 @@
 # scons co=1 b=GccOpt ts=projects/Ozzy/test/Potts/TestPottsMutations.hpp
 #
 
-start_run=11
-end_run=100
+start_run=0
+end_run=1000
 
-for (( i=start_run ; i<end_run; i = i+25))
+for (( i=start_run ; i<end_run; i = i+125))
 do
     echo "Beginning run $i"
     # NB "nice -20" gives the jobs low priority (good if they are going to dominate and and no slower if nothing else is going on)
